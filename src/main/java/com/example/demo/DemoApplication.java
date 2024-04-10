e1fa2bf194c6253d823b35ecb2f49f578b6dd172
Cristian <cristian.piedrahita88@gmail.com>
2024-04-09 19:23:50 -0500

    Path

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-		System.out.print("Sergio por que me odias");
-
 	}
+
+	@GetMapping("/saludo/{nombre}")
+    public String obtenerSaludo(@PathVariable String nombre) {
+        return "Hola, " + nombre + ". Bienvenido a mi aplicación!";
+    }
+	
 
 }
