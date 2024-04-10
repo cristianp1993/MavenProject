9f804e956b2caff5bf62900a13cb5890857e4a0b
Cristian <cristian.piedrahita88@gmail.com>
2024-04-09 19:02:38 -0500

    Actualizacion saludo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		System.out.print("Hello Mundo");
+		System.out.print("Hola Willinton");
 
 	}
 
