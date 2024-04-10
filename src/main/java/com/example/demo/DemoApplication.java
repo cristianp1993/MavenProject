aacab36df597836118f2a1da8dc5297aaf399cbf
Cristian <cristian.piedrahita88@gmail.com>
2024-04-09 20:14:21 -0500

    Cambio Metodo saludo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,10 +13,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	@GetMapping("/")
-    public String obtenerSaludo(@RequestParam(value = "nombre", defaultValue = "Invitado") String nombre) {
-        return "Hola, " + nombre + ". Bienvenido a mi aplicación!";
+    public void obtenerSaludo(@RequestParam(value = "nombre", defaultValue = "Invitado") String nombre) {
+		logger.info("Se ha solicitado un saludo para el nombre: " + nombre);
     }
 	
 
